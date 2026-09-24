69406983827ca5626dc1b7646936d4a467f05b08
Ana Gomes <ana.c.jesus6@edu.senai.br>
2026-09-24 11:53:28 -0300

    fist

---
+++ find().js
@@ -0,0 +1,24 @@
+// exercicio 1
+
+const produtos = [
+  { id: 1, nome: "Notebook", preco: 3500, estoque: 5, ativo: true },
+  { id: 2, nome: "Mouse", preco: 80, estoque: 0, ativo: true },
+  { id: 3, nome: "Teclado", preco: 150, estoque: 10, ativo: false },
+  { id: 4, nome: "Monitor", preco: 1200, estoque: 3, ativo: true }
+];
+
+ const procura = produtos.find((p) => p.id === 3 );
+ console.log(procura);
+
+//exercicio 2
+
+const produto1 = [
+  { id: 1, nome: "Notebook", preco: 3500, estoque: 5, ativo: true },
+  { id: 2, nome: "Mouse", preco: 80, estoque: 0, ativo: true },
+  { id: 3, nome: "Teclado", preco: 150, estoque: 10, ativo: false },
+  { id: 4, nome: "Monitor", preco: 1200, estoque: 3, ativo: true }
+];
+
+const estoque = produto1.find((produto1) => produto1 >= 0);
+console.log("Primeiro zero >= 0:", estoque);
+console.log("\n");
